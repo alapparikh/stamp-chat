9c0d21a2f7b5c33a67e4530d39c1ac00b8440536
Tim Novikoff <timnovikoff@rrdhcp-10-32-158-13.redrover.cornell.edu>
2014-01-25 13:23:00 -0500

    Initial Commit

---
+++ StampChat/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  StampChat
+//
+//  Created by Tim Novikoff on 1/25/14.
+//  Copyright (c) 2014 Tim Novikoff. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
